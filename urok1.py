5020ba304225105201c85c9415f63d5bfdb51aca
Yarik228335 <116014224+Yarik228335@users.noreply.github.com>
2022-10-23 21:42:38 +0300

    Add files via upload

---
+++ urok1.py
@@ -0,0 +1,25 @@
+""
+import biblioteka
+from biblioteka import *
+""
+
+def func():
+    a = 1000
+    while a > 0:
+        a = a - 7
+        print(a)
+
+func()
+peremennaya = func()
+print(func())
+
+list = [1, 2, 3]
+masiv = {}
+list.append(4)
+print(list[3])
+
+a = int(float(0.333333333))
+b = str()
+c = bool()
+        
+    
